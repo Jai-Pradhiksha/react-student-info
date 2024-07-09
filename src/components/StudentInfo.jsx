24f45bdd40003df97dc62294ebed2c2cda67e7bc
Jai-Pradhiksha <pradhikshajai@gmail.com>
2024-07-09 15:08:13 +0530

    Eye

--- src/components/StudentInfo.jsx
+++ src/components/StudentInfo.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './StudentInfo.css'
+import EyeIcon from './EyeIcon';
 
 const StudentInfo = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -12,6 +13,10 @@ const StudentInfo = () => {
   return (
     <div className=" min-vw-100 position-relative min-vh-100 d-flex justify-content-center align-items-center animate-bgChange">
       <div className="card p-4 shadow-lg text-center">
+        <div className='justify-content-center align-items-center'>
+          <EyeIcon/>
+        </div>
+        
         <h1 className="text-2xl font-bold mb-3">Name: Jai Pradhiksha</h1>
         <p className="text-gray-600 mb-3">Registration No: 123456</p>
         <button
@@ -31,3 +36,4 @@ const StudentInfo = () => {
 };
 
 export default StudentInfo;
+
